8bc94df90a8e33698150d37f448ef5673b621529
swayam-rajput <115392841+swayam-rajput@users.noreply.github.com>
2024-12-23 14:49:48 +0530

    Update Timer.tsx

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -55,7 +55,7 @@ const Timer = () => {
             // return mode === 'work' ? worktime : breaktime;
         }
     }
-    const formatTime = (time) => {
+    const formatTime = (time:number) => {
         const minutes = Math.floor(time / 60);
         const seconds = time % 60;
         return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
